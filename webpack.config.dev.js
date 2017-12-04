ad85e85b2d16efcedc32d6df6807103ced4aef75
sulenchy <sulaimanabiodun172@gmail.com>
2017-12-04 18:32:00 +0100

    refactor

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,6 @@
-import path from 'path';
+const path = require('path');
 
-export default {
+module.exports = {
   devtool: 'inline-source-map',
   entry: [
    path.resolve(__dirname, './client/index.js'),
